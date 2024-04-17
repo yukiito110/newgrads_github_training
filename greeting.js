1dd4b29895c46d3411d40ac7705022fedae63931
maekawa.naoki <maekawa.naoki@KMPC-029.local>
2024-04-17 13:20:05 +0900

    Add greeting varieties for time.

--- greeting.js
+++ greeting.js
@@ -1,11 +1,41 @@
 function greet(name, language) {
+  let d = new Date();
+  let hours = d.getHours();
   var greeting = "Hello";
   if (language === "es") {
-      greeting = "Hola";
+      if (hours < 12){
+        greeting = "Buenos días";
+      }
+      else if (hours < 18){
+        greeting = "Buenas tardes";
+      }
+      else {
+        greeting = "Buenas noches";
+      }
   } else if (language === "fr") {
       greeting = "Bonjour";
+      if (hours < 12){
+        greeting = "Bonjour";
+      }
+      else if (hours < 18){
+        greeting = "Bon après-midi";
+      }
+      else {
+        greeting = "Bonsoir";
+      }
+  }
+  else{
+    if (hours < 12){
+      greeting = "Good morning";
+    }
+    else if (hours < 18){
+      greeting = "Good afternoon";
+    }
+    else {
+      greeting = "Good evening";
+    }
   }
   console.log(greeting + ", " + name + "!");
 }
 
-greet("World", "es");
+greet("World", "fr");
