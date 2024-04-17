33560ee68a828cd09bd4af5a33c7929492f81875
yoyon-cha <140133727+yoyon-cha@users.noreply.github.com>
2024-04-17 13:51:58 +0900

    Resolve conflicts and add multilingual greeting functionality

--- greeting.js
+++ greeting.js
@@ -1,11 +1,20 @@
 function greet(name, language) {
+  var currentHour = new Date().getHours();
   var greeting = "Hello";
+  var timeOfDay = currentHour < 12 ? "morning" : currentHour < 18 ? "afternoon" : "evening";
+  var translatedGood = "Good";
+  var totalGreeting = greeting + "! " + translatedGood + " " + timeOfDay + ", " + name + "!";
   if (language === "es") {
       greeting = "Hola";
+      translatedGood = "Buenas";
+      timeOfDay = currentHour < 12 ? "días" : currentHour < 18 ? "tardes" : "noches";
+      totalGreeting = greeting + "! " + translatedGood + " " + timeOfDay + ", " + name + "!";
   } else if (language === "fr") {
-      greeting = "Bonjour";
+      greeting = "Bon";
+      timeOfDay = currentHour < 12 ? "jour" : currentHour < 18 ? "jour" : "soir";
+      totalGreeting = greeting + timeOfDay + ", " + name + "!";
   }
-  console.log(greeting + ", " + name + "!");
+  console.log(totalGreeting);
 }
 
 greet("World", "es");
