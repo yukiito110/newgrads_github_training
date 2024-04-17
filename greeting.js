cf5a843ea56077ed664045fdd8f1233155044bc8
Wakana084 <wakana.miyazaki@kufu.co.jp>
2024-04-17 13:59:00 +0900

    Change language by entered langage

--- greeting.js
+++ greeting.js
@@ -1,7 +1,14 @@
 function greet(name) {
   var currentHour = new Date().getHours();
   var timeOfDay = currentHour < 12 ? "morning" : currentHour < 18 ? "afternoon" : "evening";
-  console.log("Good " + timeOfDay + ", " + name + "!");
+  if (language === "es") {
+    greeting = "Hola";
+  } else if (language === "fr") {
+    greeting = "Bonjour";
+  } else {
+    greeting = "Good " + timeOfDay;
+  }
+  console.log(greeting + ", " + name + "!");
 }
 
-greet("World");
+greet("World", "es");
